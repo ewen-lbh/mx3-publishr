c0f3c117cad214213651a60a32e9415c481c0e61
ewen-lbh <ewen.lebihan7@gmail.com>
2019-01-30 15:31:40 +0100

    removed silly test things from main, updated gitignore

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import consts
 from utils import *
 from pprint import pprint
 
+log.watermark()
+
 if consts.ENV != 'dev': 
     log.fatal('Whoops! \nScript still in construction! \nCome back later!')
-
-log.watermark()
-string='orjeog Ijire eofk ! 76  \n\t ejfoi'
-log.fatal(sentence(string))
