67d8317dda0d0c8b4415cad1376a0f4806ffc7db
ewen-lbh <ewen.lebihan7@gmail.com>
2019-02-01 18:24:31 +0100

    fixed Dirs.__init__() (forgot slash in path construction)

--- Data_Dirs.py
+++ Data_Dirs.py
@@ -3,6 +3,6 @@ from consts import *
 class Dirs:
     def __init__(self, parentself):
         self.parent = parentself
-        self.audio = BASEPATHS['audio']+str(self.parent.collection)
-        self.cover = BASEPATHS['cover']+str(self.parent.collection)
-        self.video = BASEPATHS['video']+str(self.parent.collection)
+        self.audio = BASEPATHS['audio']+'/'+str(self.parent.collection)
+        self.cover = BASEPATHS['cover']+'/'+str(self.parent.collection)
+        self.video = BASEPATHS['video']+'/'+str(self.parent.collection)
