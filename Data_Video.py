bbb51110e9e6aa1b7729208871dcde1588f4dbb1
ewen-lbh <ewen.lebihan7@gmail.com>
2019-02-01 19:26:01 +0100

    struct: grouped imports into a single file

--- Data_Video.py
+++ Data_Video.py
@@ -1,4 +1,8 @@
+from imports import *
+
 class Video:
     def __init__(self, parentself):
         self.parent = parentself
-    
+
+    def missing(self):
+        
---
+++ imports.py
@@ -0,0 +1,6 @@
+# import as globals
+from utils import *
+from cli import *
+from consts import *
+from Data import *
+from pprint import pprint
