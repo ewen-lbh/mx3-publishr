0b0cf0320bbf0b2fdc4d735b55c897404422a7e6
ewen-lbh <ewen.lebihan7@gmail.com>
2019-02-10 10:12:35 +0100

    struct: moved watermark out of consts.py

--- consts.py
+++ consts.py
@@ -265,62 +265,10 @@ if SPECIAL_CHARS_COMPATIBILITY_LEVEL >= 2:
     SPECIAL_CHARS['ox'] = '⦻'
 
 # === WATERMARK ===
+import watermark
+WATERMARK_LOGO_FONT = 'ogre'
+WATERMARK = watermark.text
 
-
-def _list_logvariants():
-    logvariants = str()
-    for k in LOG_TYPES:
-        v = LOG_TYPES[k]
-        orig_k = k
-        k = k.title()
-        if k == 'Debug' and not VERBOSE_OUTPUT:
-            k += ' (deactivated)'
-        logvariants += f'{CLI_STYLING_CODES[LOG_TYPES_COLORS[orig_k]]} - "{LOG_TYPES_WRAP[0]+str(v)+LOG_TYPES_WRAP[1]}" {k}{CLI_STYLING_CODES["ENDC"]}\n'
-    return logvariants
-
-
-# TODO move this out of consts
-# TODO fetch github description to put it here ? (use a consts to toggle that or show fallback description)
-WATERMARK_FONT = 'ogre'
-WATERMARK = pyfiglet.figlet_format("publishr", font=WATERMARK_FONT)+"""
-Get music out, without trouble.
-
-                             
-{headerc}WEBSITE{c}
-mx3creations.com
-
-{headerc}REPOSITORY{c}
-github.com/ewen-lbh/mx3-publishr
-            
-{headerc}LOG TYPES{c}
-Each message is preceded by {logwrap_l}x{logwrap_r}.
-The [x] indicates the type of message you're getting.
-            
-Here's the list of all characters:
-{logvariants_list}
-            
-In the same way, "{userinput}" indicates that 
-user input is requested.
-
-{headerc}CONFIGURATION{c}              
-To change something in the config, you can use the (work in progress) config wizard.
-Type {cc}/config{c} at any time to open it, and {cc}/close{c} to close it.
-Note that when you close the config wizard, the script reruns from the beginning.
-            
-{rArr}  Let's start!
-            
-""" .format(
-    logwrap_l=LOG_TYPES_WRAP[0],
-    logwrap_r=LOG_TYPES_WRAP[1],
-    userinput=USER_INPUT_INDICATOR.strip(),
-    rArr=SPECIAL_CHARS['rArr'],
-    logvariants_list=_list_logvariants(),
-    cc=CLI_STYLING_CODES['CYAN'],
-    c=CLI_STYLING_CODES['ENDC'],
-    headerc=CLI_STYLING_CODES[SECTION_COLOR]
-)
-
-del _list_logvariants
 
 SCRIPT_END_MESSAGE = """Thank you for using this script !
 
